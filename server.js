3397a71a01a670f150360af415ae8c158b6ea0f0
Peshal <ppeshalmani@gmail.com>
2025-01-29 00:11:55 +0545

    Removed Client from being tracked by Git

---
+++ server.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const path = require("path")
+
+const connectDb = require("./utils/db");
+
+const PORT = process.env.PORT || 8000;
+
+app.use(express.json());
+
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static("client/dist"))
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
+
+connectDb().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
+});
